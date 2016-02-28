a4d7e6df5cef846e48f4241ba551f0bc95a731d1
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-27 21:52:34 -0500

    add any card target

--- game/targeting-functions.js
+++ game/targeting-functions.js
@@ -1,12 +1,18 @@
 function inactivePlayerInPlay(game, targetId) {
-	var card = game.zones.getZone('shared:player-'+(game.activePlayer?0:1)+'-inplay').getCard(targetId);
+	var card = game.zones.getZone('shared:player-'+(game.getInactivePlayer())+'-inplay').getCard(targetId);
 	return card;
 }
 function inPlay(game, targetId) {
 	var card = game.zones.getZone('shared:player-'+game.activePlayer+'-inplay').getCard(targetId);
 	return card;
 }
+function anyCreature(game,targetId) {
+	var card = game.zones.getZone('shared:player-'+game.activePlayer+'-inplay').getCard(targetId);
+	if (!card) { card = game.zones.getZone('shared:player-'+game.getInactivePlayer()+'-inplay').getCard(targetId); }
+	return card;
+}
 module.exports = {
 	'player-(opponent)-inplay': inactivePlayerInPlay,
-	'player-(self)-inplay': inPlay
+	'player-(self)-inplay': inPlay,
+	'inplay': anyCreature
 };
